35551131b76fec66de5b56d1c4c5e429bbac2571
utcpret <ndeye-aminata-aida.faye@utc.etu.fr>
2026-06-13 16:58:16 +0200

    modèle régression logistique corrigée

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,7 @@
 from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
 def train_and_evaluate(model, X_train, y_train, X_test, y_test):
     """
@@ -9,10 +12,28 @@ def train_and_evaluate(model, X_train, y_train, X_test, y_test):
     y_train_pred = model.predict(X_train)
     y_test_pred = model.predict(X_test)
 
-    print("=== TRAIN ===")
+    print("--------- Métriques pour TRAIN ---------")
     print(classification_report(y_train, y_train_pred))
 
-    print("=== TEST ===")
+    print("--------- Métriques pour TEST ---------")
     print(classification_report(y_test, y_test_pred))
 
     return model
+
+
+def plot_confusion_matrix(y_true, y_pred, title="Matrice de confusion"):
+    """
+    Affiche une matrice de confusion avec heatmap seaborn.
+    """
+    cm = confusion_matrix(y_true, y_pred)
+
+    plt.figure(figsize=(5,4))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+
+    plt.xlabel("Prédit")
+    plt.ylabel("Réel")
+    plt.title(title)
+
+    plt.show()
+
+    return cm
